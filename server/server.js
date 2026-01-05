44fc7aad2af1a652f9d0f9bab29ec8bc6576cc99
Spencer Endriss <111301503+AlpineLobster79@users.noreply.github.com>
2026-01-05 07:41:39 -0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -14,11 +14,11 @@ app.use(cors());
 //app.use(cors({ origin: "https://pe139.brighton.domains" }));    //restricted to my frontend domain
 app.use(express.json());    //parse JSON requests
 
-//server uploaded imgs
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-
 //routes
 app.use("/api/sightings", sightingsRouter);
+
+//server uploaded imgs
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 //default route
 app.get("/", (req, res) => {
@@ -30,10 +30,18 @@ if (process.env.NODE_ENV === "production") {
     const buildPath = path.join(__dirname, "../zapp/build");
     app.use(express.static(buildPath));
     //app.use(express.static(path.join(__dirname, "../zapp/build")));
-    app.get("/*", (req, res) => {
-        //res.sendFile(path.resolve(__dirname, "../zapp/build", "index.html"))
-        res.sendFile(path.join(buildPath, "index.html"));
+
+    app.get((req, res, next) => {
+        if (!req.path.startsWith("/api") && !req.path.startsWith("/uploads")) {
+            res.sendFile(path.join(buildPath, "index.html"));
+        } else {
+            next();
+        }
     });
+    //app.get("/*", (req, res) => {
+        //res.sendFile(path.resolve(__dirname, "../zapp/build", "index.html"))
+        //res.sendFile(path.join(buildPath, "index.html"));
+    //});
 }
 
 //error handling middleware
@@ -47,4 +55,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
 
