a1ccd0699375fc016cc61b32db5a5036f4d6b09e
Spencer Endriss <111301503+AlpineLobster79@users.noreply.github.com>
2026-01-05 13:14:48 -0600

    connectDB.js vars match railway vars

--- server/database/connectDB.js
+++ server/database/connectDB.js
@@ -2,11 +2,11 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT || 3306,
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: Number(process.env.MYSQLPORT),
     //connection limits & timeout to prevent crashes
     waitForConnections: true,
     connectionLimit: 10,
@@ -15,3 +15,4 @@ const pool = mysql.createPool({
 
 
 module.exports = pool;
+
